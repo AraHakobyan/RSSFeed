d921acd2e0742daef2db17ddeb31141dcf4a7f39
ara94 <arahakobyangg@gmail.com>
2020-12-23 18:15:58 +0400

    - Added rss dependency - Created Feed item ui base part

---
+++ app/src/main/java/am/develop/rssfeed/application/feed/FeedActivity.kt
@@ -0,0 +1,20 @@
+package am.develop.rssfeed.application.feed
+
+import am.develop.rssfeed.R
+import am.develop.rssfeed.base.view.BaseActivity
+
+/**
+ * Created by Ara Hakobyan on 12/23/2020.
+ * Company IDT
+ */
+class FeedActivity : BaseActivity<FeedActivityViewModel>(){
+    override fun onCreateView(): Int = R.layout.activity_feed
+
+    override fun initViewModel() {
+
+    }
+
+    override fun setupView() {
+
+    }
+}
